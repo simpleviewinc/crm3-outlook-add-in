9ab69f5b4f7d2e78d15da9625c42127e4e4c077c
Shailesh Pandey <shailesh@digitalmettle.com>
2024-05-16 16:26:48 +0530

    added static UI pages

---
+++ OutlookToolbarWeb/Pages/SyncEmail/SyncEmail.js
@@ -0,0 +1,20 @@
+﻿// Function to toggle the state of all checkboxes in a box
+function toggleCheckboxes(boxId, isChecked) {
+    var checkboxes = document.querySelectorAll('#' + boxId + ' .row-checkbox');
+    checkboxes.forEach(function (checkbox) {
+        checkbox.checked = isChecked;
+    });
+}
+
+// Function to handle the "Select All" checkbox change
+function handleSelectAllCheckbox(boxId) {
+    var selectAllCheckbox = document.querySelector('#' + boxId + ' .select-all-checkbox');
+    toggleCheckboxes(boxId, selectAllCheckbox.checked);
+}
+
+// Function to handle the "Select All" button click
+function handleSelectAllButton(boxId) {
+    toggleCheckboxes(boxId, true);
+    var selectAllCheckbox = document.querySelector('#' + boxId + ' .select-all-checkbox');
+    selectAllCheckbox.checked = true;
+}
