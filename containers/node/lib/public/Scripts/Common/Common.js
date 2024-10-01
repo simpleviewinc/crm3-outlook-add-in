ddf1e65ee43ca27332cee16dac194e68f28ff00c
Jason Mac Entee <jason@digitalmettle.com>
2024-10-01 12:20:53 -0400

    Working to clean up so setting the crm URL and the API url (the proxy server with the crm URL appended) is more consistant.

--- containers/node/lib/public/Scripts/Common/Common.js
+++ containers/node/lib/public/Scripts/Common/Common.js
@@ -33,4 +33,61 @@ function GetDataFromLocalStorage() {
 	return data;
 }
 
+function GetCrmUrlFromLocalStorage() {
+	let resval = localStorage.getItem("CRM");
+	let data = {};
+	if (resval != null) {
+		data = decodeFromBase64(resval);
+		let url = data.crmUrl;
+
+		if (url.endsWith('/')) {
+			url = url.slice(0, -1);
+		}
+
+		if (url.endsWith(".simpleviewcrm.com")) {
+			return url;
+		}
+	}
+	alert("Url " + url + " not valid");
+	return;
+}
+
+
+function ValidateCRMUrl(url) {
+	if (url.endsWith(".simpleviewcrm.com")) {
+		return true;
+	} else {
+		alert("Url " + url + " not valid");
+		return false;
+	}
+}
+
+
+function GetProxyUrl(url) {
+	//if (window.location.hostname.toLowerCase().indexOf('localhost') > -1 ||
+	//	window.location.hostname.toLowerCase().indexOf('.vdev') > -1) {
+
+	//	if (url === "https://demo.simpleviewcrm.com") {
+	//		if (window.location.hostname.toLowerCase().indexOf('localhost') > -1) {
+	//			url = "http://localhost:4000/api";
+	//		} else if (window.location.hostname.toLowerCase().indexOf('.vdev') > -1) {
+	//			url = "https://c219-13-84-216-53.ngrok-free.app/api";
+	//		}
+	//	} else {
+	//		alert("Url not valid");
+	//		url = '';
+	//	}
+
+	//} else {
+	//	if (url.endsWith(".simpleviewcrm.com")) {
+	//		return url; // use the actual url provided by user
+	//	} else {
+	//		alert("Url not valid");
+	//		url = '';
+	//	}
+	//}
+	return '/submit/?apiUrl='+url;
+}
+
+
 window.GetDataFromLocalStorage = GetDataFromLocalStorage;
