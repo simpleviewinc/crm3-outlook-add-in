e8d0134c29948acdda672ca9a3a7b02b4f0f4d2c
Shailesh Pandey <shailesh@digitalmettle.com>
2024-11-22 11:33:43 +0530

    Changes related to #22236

--- containers/node/lib/public/Pages/Settings/Settings.js
+++ containers/node/lib/public/Pages/Settings/Settings.js
@@ -80,24 +80,24 @@ $(document).ready(function () {
 				//console.log(decodedString);
 				if (decodedString == '-1.0') {
 					$("#settingLoader").hide();
-					alert("Credentials not valid.");
+					createDialog("Credentials not valid.", function() {});
 				}
 				else {
 					$("#settingLoader").hide();
-					alert("Login successful.");
-					UserId = parseInt(decodedString);
-					$('#emailSettings').show();
-					$('#Submit').hide();
-					$('#logout').show();
-					ApiUrl = GetProxyUrl(crmUrl);
-					GetTaskTypes();
-					GetPriorityType();
+					createDialog("Login successful.", function() {
+						UserId = parseInt(decodedString);
+						$('#emailSettings').show();
+						$('#Submit').hide();
+						$('#logout').show();
+						ApiUrl = GetProxyUrl(crmUrl);
+						GetTaskTypes();
+						GetPriorityType();
+					});
 				}
 			})
 			.fail(function (jqXHR, textStatus, errorThrown) {
 				$("#settingLoader").hide();
-				alert("Url or credentials not valid.");
-				console.error('Error:', textStatus, errorThrown);
+				createDialog("Url or credentials not valid.", function() {console.error('Error:', textStatus, errorThrown);});
 			});
 	}
 
@@ -301,42 +301,45 @@ $(document).ready(function () {
 		console.log('setting localStorage');
 		localStorage.setItem("crm", formDataEncodeString);
 
-		alert("Settings updated.");
-		if (window.opener && !window.opener.closed) {
-			if (typeof window.opener.ReloadTaskPane === 'function') {
-				console.log('window.opener.CloseTheTaskPane');
-				window.opener.ReloadTaskPane(false);
-				//window.close(); // Optionally close the popup after sending data
+		
+		createDialog("Settings updated.", function() {
+			if (window.opener && !window.opener.closed) {
+				if (typeof window.opener.ReloadTaskPane === 'function') {
+					console.log('window.opener.CloseTheTaskPane');
+					window.opener.ReloadTaskPane(false);
+					//window.close(); // Optionally close the popup after sending data
+				} else {
+					console.error("Parent window method setCategoryToEmail is not defined.");
+				}
+				if (typeof window.opener.SetLocalStorageItem === 'function') {
+					console.log('window.opener.CloseTheTaskPane');
+					window.opener.SetLocalStorageItem(formDataString);
+					//window.close(); // Optionally close the popup after sending data
+				} else {
+					console.error("Parent window method SetLocalStorageItem is not defined.");
+				}
+	
 			} else {
-				console.error("Parent window method setCategoryToEmail is not defined.");
+				console.error("Parent window is not available.");
 			}
-			if (typeof window.opener.SetLocalStorageItem === 'function') {
-				console.log('window.opener.CloseTheTaskPane');
-				window.opener.SetLocalStorageItem(formDataString);
-				//window.close(); // Optionally close the popup after sending data
-			} else {
-				console.error("Parent window method SetLocalStorageItem is not defined.");
-			}
-
-		} else {
-			console.error("Parent window is not available.");
-		}
+		});
 	});
 
 	$("#reset").click(function () {
 		localStorage.removeItem('crm');
-		alert("Settings removed.");
-		window.location.reload();
-		if (window.opener && !window.opener.closed) {
-			if (typeof window.opener.ReloadTaskPane === 'function') {
-				console.log('window.opener.CloseTheTaskPane');
-				window.opener.ReloadTaskPane(true);
-				//window.close(); // Optionally close the popup after sending data
+		createDialog("Settings removed.", function() {
+			window.location.reload();
+			if (window.opener && !window.opener.closed) {
+				if (typeof window.opener.ReloadTaskPane === 'function') {
+					console.log('window.opener.CloseTheTaskPane');
+					window.opener.ReloadTaskPane(true);
+					//window.close(); // Optionally close the popup after sending data
+				} else {
+					console.error("Parent window method setCategoryToEmail is not defined.");
+				}
 			} else {
-				console.error("Parent window method setCategoryToEmail is not defined.");
+				console.error("Parent window is not available.");
 			}
-		} else {
-			console.error("Parent window is not available.");
-		}
+		});
 	});
 });
